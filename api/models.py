3f41c0fdfda1d5e0ce2c8afa551005b550973158
farzinsh7 <farzinnater@gmail.com>
2024-09-15 15:16:26 +0330

    Create first api view(function base)

--- api/models.py
+++ api/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class UserProfile(models.Model):
     name = models.CharField(max_length=300)
     age = models.PositiveIntegerField()
-    bio = models.TimeField()
+    bio = models.TextField()
     email = models.EmailField()
     
     def __str__(self):
--- api/views.py
+++ api/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from . import serializers
+from . import models
 
-# Create your views here.
+
+@api_view(['GET', 'POST'])
+def user_profile(request):
+    
+    data = models.UserProfile.objects.all()
+    serializer_data = serializers.UserProfileSerializer(data, many=True)
+    
+    return Response(serializer_data.data)
+    
+
