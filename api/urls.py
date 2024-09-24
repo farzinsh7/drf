923a3439a09df16c80c5c1a5cd019b5f5d252d16
farzinsh7 <farzinnater@gmail.com>
2024-09-24 14:47:48 +0330

    Start new project

--- api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('user/', views.UserProfileList.as_view(), name="user-profile"),
-    path('user/<int:pk>/', views.UserProfileDetail.as_view(), name="update-user-profile"),
-]
--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for config project.
+URL configuration for core project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
@@ -15,9 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('api.urls')),
 ]
