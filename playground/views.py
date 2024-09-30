7641fafdc9754be03f7ced9c92bc9de48a0d2c73
farzinsh7 <farzinnater@gmail.com>
2024-09-30 14:55:57 +0330

    Create simple view

--- playground/views.py
+++ playground/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from store.models import Product
 
 # Create your views here.
+def say_hello(request):
+    products = Product.objects.all()
+    
+    return render(request, 'index.html', )
