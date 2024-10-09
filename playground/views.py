96bb5cc0c5387054c1abaae9f17f6f8ab50c12d8
farzinsh7 <farzinnater@gmail.com>
2024-10-09 09:47:22 +0330

    Filtering Objects

--- playground/views.py
+++ playground/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
+from django.core.exceptions import ObjectDoesNotExist
 from store.models import Product
 
 # Create your views here.
 def say_hello(request):
     
-    product = Product.objects.get(pk=1)
-    
-    return render(request, 'hello.html', {'name' : 'Farzin'})
+    queryset = Product.objects.filter(unit_price__range=(20, 30))
+        
+    return render(request, 'hello.html', {'name' : 'Farzin', 'products': list(queryset)})
