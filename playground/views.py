6b7bf1f707937991853519d19a7f46798c646125
farzinsh7 <farzinnater@gmail.com>
2024-10-09 11:14:09 +0330

    Get count of order items by specific customer

--- playground/views.py
+++ playground/views.py
@@ -5,11 +5,7 @@ from store.models import Product, Order, OrderItem
 # Create your views here.
 def say_hello(request):
     
-    orders = Order.objects.filter(customer__id=1)
-    
-    order_items = OrderItem.objects.filter(order__in=orders).select_related('product')
-    
-    product_titles = [item.product.title for item in order_items]
+    orders = OrderItem.objects.filter(product__collection__id=3).count()
     
         
-    return render(request, 'hello.html', {'name' : 'Farzin', 'products': product_titles})
+    return render(request, 'hello.html', {'name' : 'Farzin', 'products': orders})
