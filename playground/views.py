c178f57ff417b2f07177064620cbe7c33acc5e4b
Farzin Shams <farzinnater@gmail.com>
2024-12-14 18:29:55 +0330

    Testing Lookup Types

--- playground/views.py
+++ playground/views.py
@@ -3,9 +3,10 @@ from django.core.exceptions import ObjectDoesNotExist
 from store.models import Product, Order, OrderItem
 
 # Create your views here.
+
+
 def say_hello(request):
-    
-    products = Product.objects.filter(inventory__lt=10, unit_price__lt=20)
-    
-        
-    return render(request, 'hello.html', {'name' : 'Farzin', 'products': list(products)})
+
+    orders = OrderItem.objects.filter(product__collection__id=3)
+
+    return render(request, 'hello.html', {'name': 'Farzin', 'products': list(orders)})
