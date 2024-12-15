9ff1e9aca23cbdf88faf55d3e369c3763e2dc12a
Farzin Shams <farzinnater@gmail.com>
2024-12-15 16:58:50 +0330

    Do some Annotating Exercises

--- playground/views.py
+++ playground/views.py
@@ -1,14 +1,13 @@
 from django.shortcuts import render
 from django.core.exceptions import ObjectDoesNotExist
-from django.db.models.aggregates import Count, Max, Min, Avg
-from store.models import Product, Order, OrderItem
+from django.db.models import F, Value, ExpressionWrapper, Max, Count, Sum
+from store.models import Product, Order, OrderItem, Customer, Collection
 
 # Create your views here.
 
 
 def say_hello(request):
-
-    result = Product.objects.filter(collection__id=3).aggregate(minimum_unit=Min(
-        'unit_price'), maximum_unit=Max('unit_price'), average=Avg('unit_price'))
+    result = Product.objects.annotate(best_seller=Sum(F(
+        'orderitem__unit_price') * F('orderitem__quantity'))).order_by('-best_seller')[:5]
 
     return render(request, 'hello.html', {'name': 'Farzin', 'result': result})
