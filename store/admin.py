6a269a8bb5317f05f1658d8eb689d4343fc75929
Farzin Shams <farzinnater@gmail.com>
2024-12-16 14:53:30 +0330

    add computed columns

--- store/admin.py
+++ store/admin.py
@@ -6,8 +6,14 @@ from . import models
 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title', 'unit_price']
+    list_display = ['title', 'unit_price', 'inventory_status']
     list_per_page = 20
+
+    @admin.display(ordering='inventory')
+    def inventory_status(self, product):
+        if product.inventory < 10:
+            return 'Low'
+        return 'OK'
 
 
 @admin.register(models.Customer)
