d1e36fb43770db498aa36037435f38faf1b4442c
Farzin Shams <farzinnater@gmail.com>
2024-12-16 12:20:54 +0330

    Creating Objects

--- playground/views.py
+++ playground/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from store.models import Product, Order, OrderItem, Customer, Collection
-from tags.models import TaggedItem
 
 # Create your views here.
 
 
 def say_hello(request):
-    TaggedItem.objects.get_tags_for(Product, 1)
+    collection = Collection()
+    collection.title = 'Video Games'
+    collection.featured_product = Product(
+        pk=1)  # or you can use only id like 1
+    collection.save()
 
     return render(request, 'hello.html', {'name': 'Farzin'})
