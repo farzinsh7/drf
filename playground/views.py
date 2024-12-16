ed65a9dfc6684e9dc48e5b8cb07c09b747c17197
Farzin Shams <farzinnater@gmail.com>
2024-12-16 12:54:37 +0330

    CRUD Exercises

--- playground/views.py
+++ playground/views.py
@@ -1,16 +1,28 @@
 from django.shortcuts import render
-from store.models import Product, Order, OrderItem, Customer, Collection
+from store.models import Product, Order, OrderItem, Customer, Collection, Cart, CartItem
 
 # Create your views here.
 
 
 def say_hello(request):
-    # collection = Collection(pk=11)
-    # collection.title = 'Games'
-    # collection.featured_product = None
-    # collection.save()
+    # start create a shopping cart
+    # cart = Cart()
+    # cart.save()
 
-    # or the better way
-    Collection.objects.filter(pk=11).update(title='Video Games')
+    # item1 = CartItem()
+    # item1.cart = cart
+    # item1.product_id = 1
+    # item1.quantity = 3
+    # item1.save()
+    # end create a shopping cart
+
+    # start update a quantity of a shopping cart
+    # CartItem.objects.filter(pk=1).update(quantity=6)
+    # end update a quantity of a shopping cart
+
+    # start delete a shopping cart
+    # cart = Cart(pk=1)
+    # cart.delete()
+    # end delete a shopping cart
 
     return render(request, 'hello.html', {'name': 'Farzin'})
