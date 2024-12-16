64329ecfb5238901e57670521d759bf36a02cc64
Farzin Shams <farzinnater@gmail.com>
2024-12-16 11:35:18 +0330

    Use Custom Manager for TaggedItem

--- playground/views.py
+++ playground/views.py
@@ -1,13 +1,11 @@
 from django.shortcuts import render
-from django.core.exceptions import ObjectDoesNotExist
-from django.db.models import F, Value, ExpressionWrapper, Max, Count, Sum
 from store.models import Product, Order, OrderItem, Customer, Collection
+from tags.models import TaggedItem
 
 # Create your views here.
 
 
 def say_hello(request):
-    result = Product.objects.annotate(best_seller=Sum(F(
-        'orderitem__unit_price') * F('orderitem__quantity'))).order_by('-best_seller')[:5]
+    TaggedItem.objects.get_tags_for(Product, 1)
 
-    return render(request, 'hello.html', {'name': 'Farzin', 'result': result})
+    return render(request, 'hello.html', {'name': 'Farzin'})
