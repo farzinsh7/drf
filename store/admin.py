0b706a1b63d1e18b80b4a60a3b06c5feae55b23e
Farzin Shams <farzinnater@gmail.com>
2024-12-16 14:47:44 +0330

    add ModelAdmin options

--- store/admin.py
+++ store/admin.py
@@ -2,5 +2,20 @@ from django.contrib import admin
 from . import models
 
 # Register your models here.
+
+
+@admin.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'unit_price']
+    list_per_page = 20
+
+
+@admin.register(models.Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'membership']
+    list_editable = ['membership']
+    ordering = ['first_name', 'last_name']
+    list_per_page = 20
+
+
 admin.site.register(models.Collection)
-admin.site.register(models.Product)
