0dcbaae5a0db5067515f7cee4cdd38d5e670c720
Farzin Shams <farzinnater@gmail.com>
2024-12-20 13:00:09 +0330

    Use Nested Routers
    Added auto assign for product id in current page, wrote a custom queryset for showing reviews for product that have been assigned

--- store/urls.py
+++ store/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path
-from rest_framework.routers import SimpleRouter, DefaultRouter
+from rest_framework_nested import routers
 from . import views
 
-router = DefaultRouter()
+router = routers.DefaultRouter()
 router.register('products', views.ProductViewSet)
 router.register('collections', views.CollectionViewSet)
 
+product_router = routers.NestedDefaultRouter(
+    router, 'products', lookup='product')
+product_router.register('reviews', views.ReviewViewSet,
+                        basename='product-reviews')
+
 app_name = "store"
 # URLConf
-urlpatterns = router.urls
+urlpatterns = router.urls + product_router.urls
