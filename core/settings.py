24657f5dfd9b4e9db8768ed39af7213eb3f6008d
Farzin Shams <farzinnater@gmail.com>
2024-12-22 12:57:27 +0330

    Getting The Current User
    Customizing default current user

--- core/settings.py
+++ core/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 from decouple import config
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -158,11 +159,13 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
     'AUTH_HEADER_TYPES': ('JWT',),
+    'ACCESS_TOKEN_LIFE_TIME': timedelta(days=1),
 }
 
 DJOSER = {
     'SERIALIZERS': {
         'user_create': 'core_custom.serializers.UserCreateSerializer',
+        'current_user': 'core_custom.serializers.UserSerializer',
     },
 }
 
