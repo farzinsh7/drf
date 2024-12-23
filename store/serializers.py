3122c8df415c5bae71e3b13e09c000b8246872a8
Farzin Shams <farzinnater@gmail.com>
2024-12-23 14:56:32 +0330

    Data Validation

--- store/serializers.py
+++ store/serializers.py
@@ -130,6 +130,14 @@ class OrderSeirializer(serializers.ModelSerializer):
 class CreateOrderSeirializer(serializers.Serializer):
     cart_id = serializers.UUIDField()
 
+    def validate_cart_id(self, cart_id):
+        if not Cart.objects.filter(pk=cart_id).exists():
+            raise serializers.ValidationError(
+                'No cart with the given ID was found')
+        elif CartItem.objects.filter(cart_id=cart_id).count() == 0:
+            raise serializers.ValidationError('The cart is empty.')
+        return cart_id
+
     def save(self, **kwargs):
         with transaction.atomic():
             cart_id = self.validated_data['cart_id']
