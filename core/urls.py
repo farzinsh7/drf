373031b33bee155da485f5c28e2bc24b076c1cbb
Farzin Shams <farzinnater@gmail.com>
2024-12-24 09:23:48 +0330

    Managing media files

--- core/urls.py
+++ core/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from debug_toolbar.toolbar import debug_toolbar_urls
@@ -32,3 +34,7 @@ urlpatterns = [
 ]
 
 urlpatterns += debug_toolbar_urls()
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
