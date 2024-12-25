22511a80852b2af839f3fac3e055e8160e17128d
Farzin Shams <farzinnater@gmail.com>
2024-12-25 12:37:17 +0330

    Installing Redis

--- playground/views.py
+++ playground/views.py
@@ -1,17 +1,16 @@
 from django.core.mail import EmailMessage, BadHeaderError
 from django.shortcuts import render
 from templated_mail.mail import BaseEmailMessage
-# Create your views here.
 
 
 def say_hello(request):
-    try:
-        message = BaseEmailMessage(
-            template_name='emails/hello.html',
-            context={'name': 'Farzin'}
-        )
-        message.send(to=['John@gmail.com'])
-    except BadHeaderError:
-        pass
+    # try:
+    #     message = BaseEmailMessage(
+    #         template_name='emails/hello.html',
+    #         context={'name': 'Farzin'}
+    #     )
+    #     message.send(to=['John@gmail.com'])
+    # except BadHeaderError:
+    #     pass
 
     return render(request, 'hello.html', {'name': 'Farzin'})
