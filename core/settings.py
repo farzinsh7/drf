5c5f8f76116aea68441c92745a46fe7911bc5502
Farzin Shams <farzinnater@gmail.com>
2024-12-25 10:35:17 +0330

    Sending a test email

--- core/settings.py
+++ core/settings.py
@@ -190,7 +190,6 @@ AUTH_USER_MODEL = 'core_custom.User'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'localhost'
 EMAIL_PORT = 2525
-EMAIL_USE_TLS = True
 EMAIL_HOST_USER = ''
 EMAIL_HOST_PASSWORD = ''
 DEFAULT_FROM_EMAIL = 'from@farzinshams.com'
--- playground/views.py
+++ playground/views.py
@@ -1,28 +1,14 @@
+from django.core.mail import send_mail, mail_admins, BadHeaderError
 from django.shortcuts import render
-from store.models import Product, Order, OrderItem, Customer, Collection, Cart, CartItem
 
 # Create your views here.
 
 
 def say_hello(request):
-    # start create a shopping cart
-    # cart = Cart()
-    # cart.save()
-
-    # item1 = CartItem()
-    # item1.cart = cart
-    # item1.product_id = 1
-    # item1.quantity = 3
-    # item1.save()
-    # end create a shopping cart
-
-    # start update a quantity of a shopping cart
-    # CartItem.objects.filter(pk=1).update(quantity=6)
-    # end update a quantity of a shopping cart
-
-    # start delete a shopping cart
-    # cart = Cart(pk=1)
-    # cart.delete()
-    # end delete a shopping cart
+    try:
+        send_mail('subject', 'message', 'info@drf.com',
+                  ['bob@farzinshams.com'])
+    except BadHeaderError:
+        pass
 
     return render(request, 'hello.html', {'name': 'Farzin'})
