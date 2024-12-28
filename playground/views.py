09670633f0dc57e97e93089a4876332abeba8552
farzinsh7 <farzinnater@gmail.com>
2024-12-28 15:07:14 +0330

    Caching Views

--- playground/views.py
+++ playground/views.py
@@ -1,13 +1,21 @@
-from django.core.cache import cache
 from django.shortcuts import render
+from django.views.decorators.cache import cache_page
+from django.utils.decorators import method_decorator
+from rest_framework.views import APIView
 import requests
 
 
-def say_hello(request):
-    key = 'httpbin_result'
-    if cache.get(key) is None:
+class HelloView(APIView):
+    @method_decorator(cache_page(5 * 60))
+    def get(self, request):
         response = requests.get('https://httpbin.org/delay/2')
         data = response.json()
-        cache.set(key, data)
+        return render(request, 'hello.html', {'name': 'farzin'})
 
-    return render(request, 'hello.html', {'name': cache.get(key)})
+
+# @cache_page(5 * 60)
+# def say_hello(request):
+#     response = requests.get('https://httpbin.org/delay/2')
+#     data = response.json()
+
+#     return render(request, 'hello.html', {'name': 'farzin'})
